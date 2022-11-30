649541ba1d2e70295b598f470592f5b72c0a3d5e
rerfid123 <diqksrk124@gmail.com>
2022-11-30 16:20:25 +0900

    add 설정

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8888',
+                // target: 'http://localhost:8888',
+                // target: 'http://49.50.161.55:8888',
+                target: 'http://slb-14970328.ncloudslb.com:80',
                 ws: true,
                 changeOrigin: true,
                 pathRewrite: {
